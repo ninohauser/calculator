1b789898fc9ccb61f4b873214d026ba0ea141d82
5ia15nihauser <5ia15nihauser@gibwintra.ch>
2017-12-18 09:08:28 +0100

    Added test cases

--- src/test/java/com/nhauser/CalculatorTest.java
+++ src/test/java/com/nhauser/CalculatorTest.java
@@ -7,8 +7,27 @@ import org.junit.Test;
 public class CalculatorTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testSummeZweiPositiv() {
+		Calculator cal = new Calculator();
+		int result = cal.addition(1, 2);
+		
+		assertEquals(3, result);
+	}
+
+	@Test
+	public void testSummeZweiNegativ() {
+		Calculator cal = new Calculator();
+		int result = cal.addition(-2, -3);
+		
+		assertEquals(-5, result);
+	}
+
+	@Test
+	public void testSummeZweiNull() {
+		Calculator cal = new Calculator();
+		int result = cal.addition(0, 0);
+		
+		assertEquals(0, result);
 	}
 
 }
